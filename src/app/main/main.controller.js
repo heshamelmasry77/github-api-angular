3f3cc896c697902f8e579e476a50cce8abea52d2
heshamelmasry77 <heshamelmasry@outlook.com>
2017-08-15 14:21:09 +0200

    Finalizing the app
    Now I can show the name, description, link and the object is available
    for more data if we required it.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,7 +9,7 @@
   function MainController($http) {
     var vm = this;
     vm.username = "heshamelmasry77";
-    $http.get("https://api.github.com/users/" + vm.username)
+    $http.get("https://api.github.com/users/" + vm.username+"/repos")
       .success(function (data) {
         vm.userData = data;
         console.log(vm.userData);
